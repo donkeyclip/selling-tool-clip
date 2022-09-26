5856fa2c3ab01f0888b1a7fe0ff79f653a59623c
ElisavetAmp <elisabet.ampatzidou@gmail.com>
2022-09-26 23:11:38 +0300

    fix:update images

--- clip/initParams.js
+++ clip/initParams.js
@@ -14,19 +14,19 @@ export default [
       },
       products:[
         {
-          image:"assets/picture-bicycle.png",
+          image:"https://donkeyclip.github.io/selling-tool-clip/server/assets/picture-bicycle.png",
           name:"Bicycle",
           description:"A bicycle for use in the city as well as on passable dirt roads. Number of gears: 21/Wheel size: 28 inches",
           price:"599€"
         },
         {
-          image:"assets/skateboard.png",
+          image:"https://donkeyclip.github.io/selling-tool-clip/server/assets/skateboard.png",
           name:"Skateboard",
           description:"A skateboard with dimensions of 78.89cm doublekick.Design on the bottom side, non-slip surface on the top.",
           price:"75€"
         },
         {
-          image:"assets/helmet.png",
+          image:"https://donkeyclip.github.io/selling-tool-clip/server/assets/helmet.png",
           name:"Matte Black/Neon Yellow Helmet",
           description:"This helmet is designed for comfort in all conditions. Ideal for those who bike, skateboard, skate.",
           price:"56€"
